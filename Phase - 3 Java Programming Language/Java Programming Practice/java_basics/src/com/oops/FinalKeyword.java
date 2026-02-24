863bec737e71f417c9aa5fff08431049470723fd
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-02-24 22:57:41 +0530

    Commit after completing OOPS

---
+++ Phase - 3 Java Programming Language/Java Programming Practice/java_basics/src/com/oops/FinalKeyword.java
@@ -0,0 +1,28 @@
+package com.oops;
+
+final class DemoClass
+{
+	
+}
+
+class Sample extends DemoClass
+{
+	final void display()
+	{
+		
+	}
+}
+
+public class FinalKeyword  extends Sample{
+	final double pi = 3.14;
+	
+	void display()
+	{
+		
+	}
+	
+	public static void main(String args[])
+	{
+		pi = 0;
+	}
+}
--- Phase - 3 Java Programming Language/Java Programming Practice/java_basics/src/com/oops/MethodChaining.java
+++ Phase - 3 Java Programming Language/Java Programming Practice/java_basics/src/com/oops/MethodChaining.java
@@ -1,6 +1,6 @@
 package com.oops;
 
-class Student
+class Student1
 {
 	private int id;
 	private String name;
@@ -18,17 +18,17 @@ class Student
 		return department;
 	}
 	
-	public Student setId(int id) {
+	public Student1 setId(int id) {
 		this.id = id;
 		return this;
 	}
 	
-	public Student setName(String name) {
+	public Student1 setName(String name) {
 		this.name = name;
 		return this;
 	}
 	
-	public Student setDepartment(String department) {
+	public Student1 setDepartment(String department) {
 		this.department = department;
 		return this;
 	}
@@ -39,7 +39,7 @@ class Student
 public class MethodChaining {
 	public static void main(String args[])
 	{
-		Student student = new Student().setId(1).setName("Vignesh").setDepartment("CSE");;
+		Student1 student = new Student1().setId(1).setName("Vignesh").setDepartment("CSE");;
 		
 	 // Method chaining.
 		
