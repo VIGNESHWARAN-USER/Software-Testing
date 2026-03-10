dcc82ae92b1eb4147eca0755bac3e705d00af41e
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-03-10 09:08:55 +0530

    10th March

--- Phase - 3 Java Programming Language/Java Programming Practice/java_basics/src/demo/JenkinsDemo.java
+++ Phase - 3 Java Programming Language/Java Programming Practice/java_basics/src/demo/JenkinsDemo.java
@@ -1,16 +1,27 @@
 package demo;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JenkinsDemo {
-	public static void main(String[] args) {
-		
-		Scanner scanner=new Scanner(System.in);
-		
-		System.out.print("Enter a value : ");
-		String a=scanner.next();
+    public static void main(String[] args) throws IOException {
 
-		
-		System.out.println("The value is : "+a);
-		
-	}
+        BufferedReader reader = new BufferedReader(
+            new FileReader("D:\\EXPLEO SMARTCLIFF\\Phase - 3 Java Programming Language\\Java Programming Practice\\java_basics\\src\\demo\\Input.txt")
+        );
+
+        String line = reader.readLine();
+
+        String[] numbers = line.split(" ");
+
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+
+        System.out.println("The value of A is : " + a);
+        System.out.println("The value of B is : " + b);
+        System.out.println("The sum is : " + (a + b));
+
+        reader.close();
+    }
 }
