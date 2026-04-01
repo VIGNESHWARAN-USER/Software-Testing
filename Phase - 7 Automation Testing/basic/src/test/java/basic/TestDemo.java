00461a8c03c3c413716e62e62e53c2a16cdde991
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-04-01 11:00:20 +0530

    Page source stored in output file

--- Phase - 7 Automation Testing/basic/src/test/java/basic/TestDemo.java
+++ Phase - 7 Automation Testing/basic/src/test/java/basic/TestDemo.java
@@ -1,17 +1,28 @@
 package basic;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestDemo {
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		
+		BufferedWriter file = new BufferedWriter(new FileWriter("src/test/java/basic/output.txt"));
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.co.in");
-		WebElement search = driver.findElement(By.name("q"));
-		search.sendKeys("Selenium", Keys.ENTER);
-		System.out.println("Page title is: "+driver.getTitle());
+		driver.get("https://www.flipkart.com/");
+		file.write("Page title is: "+driver.getTitle());
+		file.append("\n");
+		file.append(driver.getPageSource());
+		file.append("\n");
+		file.append("URL: "+driver.getCurrentUrl());
+		file.append("\n\n");
+		file.append("EOF");
 		//driver.close();
+		file.close();
 	}
 }
