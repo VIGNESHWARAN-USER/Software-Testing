654a744829dbca1ce48c3dc50e9ed238081a55ed
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-05-22 09:07:48 +0530

    22nd MAY

---
+++ Phase - 8 Python Programming Language/Python Basics/Demo.py
@@ -0,0 +1,17 @@
+# a = 10
+# b = "Hello world!"
+# c = 10.0
+# d = {1, 2, 3}
+# e = None
+
+# print("Type of a = ", type(a))
+# print("Type of b = ", type(b))
+# print("Type of c = ", type(c))
+# print("Type of d = ", type(d))
+# print(type(a+c))
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# print(f"My name is {name}, and my age is {age}")
+
+print("Apple", "Banana", sep = "|", end = "\n")
