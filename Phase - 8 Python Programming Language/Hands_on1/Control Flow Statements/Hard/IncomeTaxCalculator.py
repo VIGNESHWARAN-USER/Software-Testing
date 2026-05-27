b493083233c6cd84ea4a4531115dcfadff005a03
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-05-28 05:16:02 +0530

    Pending conditional statements code added

--- Phase - 8 Python Programming Language/Hands_on1/Control Flow Statements/Hard/IncomeTaxCalculator.py
+++ Phase - 8 Python Programming Language/Hands_on1/Control Flow Statements/Hard/IncomeTaxCalculator.py
@@ -2,18 +2,12 @@ income = int(input("Enter income: "))
 
 if income <= 250000:
     print("You are exempted from tax")
-
 elif income <= 500000:
-    tax = (income - 250000) * 10 / 100
+    tax = (income - 250000) * 0.10
     print("Tax amount is", int(tax))
-
 elif income <= 1200000:
-    tax = 250000 * 10 / 100
-    tax = tax + (income - 500000) * 20 / 100
+    tax = 250000 * 0.10 + (income - 500000) * 0.20
     print(int(tax))
-
 else:
-    tax = 250000 * 10 / 100
-    tax = tax + 700000 * 20 / 100
-    tax = tax + (income - 1200000) * 30 / 100
+    tax = 250000 * 0.10 + 700000 * 0.20 + (income - 1200000) * 0.30
     print(int(tax))
