f643b1f8931a2b07b164cd9cce70ae90e6ec8489
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-05-29 05:04:48 +0530

    Added set and dictionary handson

---
+++ Phase - 8 Python Programming Language/Tuples and Dictionaries/NestedTuple.py
@@ -0,0 +1,9 @@
+my_tuple1 = ()
+my_tuple2 = (1, 2, 3)
+my_tuple3 = (1, "Hello", 3.4)
+my_tuple4 = ("mouse", [8, 4, 6], (1, 2, 3))
+
+print(my_tuple1)
+print(my_tuple2)
+print(my_tuple3)
+print(my_tuple4)
